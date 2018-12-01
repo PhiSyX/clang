3461c761d4e349ea9e8c3e19b6d01764b9d479a0
Mike 'PhiSyX' S. <738010+PhiSyX@users.noreply.github.com>
2018-12-01 13:56:32 +0000

    os: Hardware-Communication / Ports

--- ownos/src/kernel.cc
+++ ownos/src/kernel.cc
@@ -1,11 +1,42 @@
 #include "types.hh"
+#include "gdt.hh"
 
 void printf(char *str)
 {
     static uint16_t *video_memory = (uint16_t *)0xb8000;
 
+    static uint8_t x = 0, y = 0;
+
     for (int i = 0; str[i] != '\0'; ++i)
-        video_memory[i] = (video_memory[i] & 0xFF00) | str[i];
+    {
+        switch (str[i])
+        {
+        case '\n':
+            x = 0;
+            y++;
+            break;
+
+        default:
+            video_memory[80 * y + x] = (video_memory[80 * y + x] & 0xFF00) | str[i];
+            x++;
+            break;
+        }
+
+        if (x >= 80)
+        {
+            x = 0;
+            y++;
+        }
+
+        if (y >= 25)
+        {
+            for (y = 0; y < 25; y++)
+                for (x = 0; x < 80; x++)
+                    video_memory[80 * y + x] = (video_memory[80 * y + x] & 0xFF00) | ' ';
+            x = 0;
+            y = 0;
+        }
+    }
 }
 
 typedef void (*constructor)();
@@ -22,6 +53,8 @@ extern "C" void callConstructors()
 extern "C" void kernelMain(const void *multiboot_structure, uint32_t /*multiboot_magic*/)
 {
     printf("Hello World!");
+
+    GlobalDescriptorTable gdt;
 
     while (1)
         ;
