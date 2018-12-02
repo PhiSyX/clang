58f0b35f64bcd9687aea65912b23387c46a2cedd
Mike 'PhiSyX' S. <738010+PhiSyX@users.noreply.github.com>
2018-12-02 15:25:12 +0000

    os: Abstractions for Drivers

--- ownos/include/keyboard.hh
+++ ownos/include/keyboard.hh
@@ -1,19 +1,33 @@
+
 #ifndef __KEYBOARD_H
 #define __KEYBOARD_H
 
 #include "types.hh"
 #include "interrupts.hh"
+#include "driver.hh"
 #include "port.hh"
 
-class KeyboardDriver : public InterruptHandler
+class KeyboardEventHandler
+{
+public:
+    KeyboardEventHandler();
+
+    virtual void OnKeyDown(char);
+    virtual void OnKeyUp(char);
+};
+
+class KeyboardDriver : public InterruptHandler, public Driver
 {
     Port8Bit dataport;
     Port8Bit commandport;
 
+    KeyboardEventHandler *handler;
+
 public:
-    KeyboardDriver(InterruptManager *manager);
+    KeyboardDriver(InterruptManager *manager, KeyboardEventHandler *handler);
     ~KeyboardDriver();
     virtual uint32_t HandleInterrupt(uint32_t esp);
+    virtual void Activate();
 };
 
 #endif
