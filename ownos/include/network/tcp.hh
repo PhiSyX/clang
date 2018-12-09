350751ba4577ffc7cd4ec37c961495af6ae82c65
Mike 'PhiSyX' S. <738010+PhiSyX@users.noreply.github.com>
2018-12-09 15:20:33 +0000

    os(temp): Transmission Control Protocol (TCP)

---
+++ ownos/include/network/tcp.hh
@@ -0,0 +1,117 @@
+#ifndef __MYOS__NETWORK__TCP_H
+#define __MYOS__NETWORK__TCP_H
+
+#include <shared/types.hh>
+#include <network/ipv4.hh>
+
+namespace myos
+{
+    namespace network
+    {
+        enum TransmissionControlProtocolSocketState
+        {
+            CLOSED,
+            LISTEN,
+            SYN_SENT,
+            SYN_RECEIVED,
+
+            ESTABLISHED,
+
+            FIN_WAIT1,
+            FIN_WAIT2,
+            CLOSING,
+            TIME_WAIT,
+
+            CLOSE_WAIT,
+            LAST_ACK,
+        };
+
+        enum TransmissionControlProtocolFlag
+        {
+            FIN = 1,
+            SYN = 2,
+            RST = 4,
+            PSH = 8,
+            ACK = 16,
+            URG = 32,
+            ECE = 64,
+            CWR = 128,
+            NS = 256
+        };
+
+        struct TransmissionControlProtocolHeader
+        {
+            shared::uint16_t srcPort;
+            shared::uint16_t dstPort;
+            shared::uint32_t sequenceNumber;
+            shared::uint32_t acknowledgementNumber;
+
+            shared::uint8_t headerSize32 : 4;
+            shared::uint8_t reserved : 3;
+            shared::uint16_t flags : 9;
+
+            shared::uint16_t windowSize;
+            shared::uint16_t checksum;
+            shared::uint16_t urgentPtr;
+        } __attribute__((packed));
+
+        class TransmissionControlProtocolSocket;
+        class TransmissionControlProtocolProvider;
+
+        class TransmissionControlProtocolHandler
+        {
+        public:
+            TransmissionControlProtocolHandler();
+            ~TransmissionControlProtocolHandler();
+            virtual void HandleTransmissionControlProtocolMessage(TransmissionControlProtocolSocket *socket, shared::uint8_t *data, shared::uint16_t size);
+        };
+
+        class TransmissionControlProtocolSocket
+        {
+            friend class TransmissionControlProtocolProvider;
+
+        protected:
+            shared::uint16_t remotePort;
+            shared::uint32_t remoteIP;
+            shared::uint16_t localPort;
+            shared::uint32_t localIP;
+            shared::uint32_t sequenceNumber;
+            TransmissionControlProtocolProvider *backend;
+            TransmissionControlProtocolHandler *handler;
+
+            TransmissionControlProtocolSocketState state;
+
+        public:
+            TransmissionControlProtocolSocket(TransmissionControlProtocolProvider *backend);
+            ~TransmissionControlProtocolSocket();
+            virtual void HandleTransmissionControlProtocolMessage(shared::uint8_t *data, shared::uint16_t size);
+            virtual void Send(shared::uint8_t *data, shared::uint16_t size);
+            virtual void Disconnect();
+        };
+
+        class TransmissionControlProtocolProvider : InternetProtocolHandler
+        {
+        protected:
+            TransmissionControlProtocolSocket *sockets[65535];
+            shared::uint16_t numSockets;
+            shared::uint16_t freePort;
+
+        public:
+            TransmissionControlProtocolProvider(InternetProtocolProvider *backend);
+            ~TransmissionControlProtocolProvider();
+
+            virtual bool OnInternetProtocolReceived(shared::uint32_t srcIP_BE, shared::uint32_t dstIP_BE,
+                                                    shared::uint8_t *internetprotocolPayload, shared::uint32_t size);
+
+            virtual TransmissionControlProtocolSocket *Connect(shared::uint32_t ip, shared::uint16_t port);
+            virtual void Disconnect(TransmissionControlProtocolSocket *socket);
+            virtual void Send(TransmissionControlProtocolSocket *socket, shared::uint8_t *data, shared::uint16_t size);
+
+            virtual TransmissionControlProtocolSocket *Listen(shared::uint16_t port);
+            virtual void Bind(TransmissionControlProtocolSocket *socket, TransmissionControlProtocolHandler *handler);
+        };
+
+    }
+}
+
+#endif
